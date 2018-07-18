662cbcad49e64e5fc104042adf61d258864559aa
Denis <ddujota@internationalsafety.com>
2018-07-18 14:57:36 -0400

    starter file created

---
+++ app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(e) {
+  let pokemonList = [
+    'Tirtouga',
+    'Houndour',
+    'Ursaring',
+    'Spinda',
+    'Banette Mega',
+    'Accelgor'
+  ];
+});
