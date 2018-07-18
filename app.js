640958290dd9ffe3eed6a133567675a34489199a
Denis <ddujota@internationalsafety.com>
2018-07-18 19:33:43 -0400

    app working makes api call, can add and remove different content based on buttons

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
   const loadButtons = (pokemonList, container) => {
     // clear container before we create any ui elements
-    parent = document.getElementById(container);
+    const parent = document.getElementById(container);
     parent.innerHTML = '';
 
     pokemonList.forEach(pokemon => {
       button = document.createElement('button');
-      button.classList.add('btn', 'btn-primary');
+      button.setAttribute('type', 'button');
+      button.classList.add('btn', 'btn-dark');
       button.innerHTML = pokemon;
       button.setAttribute('data-type', pokemon);
       parent.append(button);
@@ -23,16 +24,38 @@ document.addEventListener('DOMContentLoaded', function(e) {
   };
 
   const getGhiphy = e => {
-    pokemon = e.target.textContent;
-    url = `http://api.giphy.com/v1/gifs/search?q=${pokemon}&api_key=Fqls5JhCEWL5A6ObgtJ2OdM14WHCH2f7&limit=5`;
-    fetch(`${url}`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => console.log(error));
-  };
+    e.preventDefault();
+    let pokemon = e.target.textContent;
+    parent = document.getElementById('gifContainer');
+    parent.innerHTML = ''; // clear the container first
+    let output = '';
 
+    if (pokemonList.includes(pokemon)) {
+      let url = `http://api.giphy.com/v1/gifs/search?q=${pokemon}&api_key=Fqls5JhCEWL5A6ObgtJ2OdM14WHCH2f7&limit=5`;
+      fetch(`${url}`)
+        .then(response => response.json())
+        .then(data => {
+          data.data.forEach(item => {
+            console.log(item);
+            debugger;
+            output = `<div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="${
+                item.images.fixed_height.url
+              }?text=Image cap" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">This little pokemon is rated ${
+                  item.rating
+                }</p>
+              </div>
+            </div>`;
+
+            parent.innerHTML += output;
+          });
+        })
+        .catch(error => console.log(error));
+    }
+  };
+  console.log(pokemonList);
   loadButtons(pokemonList, 'buttons');
   document.getElementById('buttons').addEventListener('click', getGhiphy);
 });
